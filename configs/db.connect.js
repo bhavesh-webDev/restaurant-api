565a52c7f2ca9ee0bd73785773c3a6416e44cbcb
bhavesh gholap <bhaveshgholap@Bhaveshs-MacBook-Air.local>
2025-08-16 20:05:06 +0530

    Modifications to test DB

--- configs/db.connect.js
+++ configs/db.connect.js
@@ -1,5 +1,9 @@
 const mongoose = require("mongoose");
 const connectDb = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 10000, // 10s
+  });
 };
 module.exports = connectDb;
